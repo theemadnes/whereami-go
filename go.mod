f32191575dc881e61719c1ba8a1bd9a36062447a
theemadnes <alex.mattson@gmail.com>
2023-03-25 05:06:52 +0000

    sorta working

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/theemadnes/whereami-go
 
 go 1.19
+
+require (
+	cloud.google.com/go/compute v1.14.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	github.com/enescakir/emoji v1.0.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.2 // indirect
+)
--- main.go
+++ main.go
@@ -3,38 +3,82 @@ package main
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"time"
+
+	"cloud.google.com/go/compute/metadata"
+	"github.com/enescakir/emoji"
 )
 
 type Payload struct {
-	PodName   string `json:"pod_name"`
-	Timestamp string `json:"timestamp"`
-	TestValue string `json:"test_value,omitempty"`
+	PodName        string `json:"pod_name"`
+	Timestamp      string `json:"timestamp"`
+	Zone           string `json:"zone,omitempty"`
+	ProjectId      string `json:"project_id,omitempty"`
+	InstanceId     string `json:"gce_instance_id,omitempty"`
+	ServiceAccount string `json:"gce_service_account,omitempty"`
+	PodNameEmoji   string `json:"pod_name_emoji,omitempty"`
 }
 
-/*func generatePayload() *Payload {
-	p := Payload{}
-	p.pod_name = "test"
-	p.timestamp = "test2"
-	return &p
-}*/
+// pick a random value from a map (used for emoji assignment)
+func pick(m map[string]string) string {
+	k := rand.Intn(len(m))
+	for _, x := range m {
+		if k == 0 {
+			return x
+		}
+		k--
+	}
+	panic("unreachable")
+}
+
+func generatePayload() Payload {
+
+	//p := Payload{}
+	payload.PodName, _ = os.Hostname()
+	payload.Timestamp = time.Now().UTC().String()
+	payload.PodNameEmoji = pick(emoji.Map())
+	//gceMetadataClient := metadata.NewClient(&http.Client{})
+
+	//projectId, gceErr := gceMetadataClient.ProjectID()
+	projectId, gceErr := metadata.ProjectID()
+	//log.Printf(projectId + "\n")
+	if gceErr != nil {
+		log.Println("Unable to capture GCE metadata")
+
+	} else {
+		payload.ProjectId = projectId
+		payload.Zone, _ = metadata.Zone()
+		payload.InstanceId, _ = metadata.InstanceID()
+		payload.ServiceAccount, _ = metadata.InstanceAttributeValue("serviceAccounts/default/email") // doesn't work for now
+	}
+	return payload
+}
+
+// laziness and creating a global payload
+var payload Payload
 
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
 	}
+	// else condition
+	log.Printf("Environment variable %s not found\n", key)
 	return fallback
 }
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
-	hostname, _ := os.Hostname() // sloppy but ignoring error
+	log.Printf("got / request\n")
+	//hostname, _ := os.Hostname() // sloppy but ignoring error
+	//projectId := getEnv()
 
 	//payload := &Payload
-	payload := Payload{PodName: hostname, Timestamp: time.Now().UTC().String()}
+	//payload := Payload{PodName: hostname, Timestamp: time.Now().UTC().String()}
+	payload = generatePayload()
+	payload.Timestamp = time.Now().UTC().String()
 	//p, _ := json.Marshal(&generatePayload())
 	w.Header().Set("Content-Type", "application/json")
 	/*p, err := json.Marshal(Payload{PodName: "test", Timestamp: "test2"})
@@ -47,14 +91,15 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
 	http.HandleFunc("/", getRoot)
 
 	err := http.ListenAndServe(":"+getEnv("PORT", "8080"), nil)
 
 	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
+		log.Printf("server closed\n")
 	} else if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
+		log.Printf("error starting server: %s\n", err)
 		os.Exit(1)
 	}
 }
